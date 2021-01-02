61be16b73dc60706bd9a9a696dc7a422d2123ba6
Jason Bongiovanni <jpbongiovanni@gmail.com>
2021-01-02 19:48:36 +0900

    after meeting with Ryan

--- reads_app/urls.py
+++ reads_app/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('add_book', views.add_book_page),
     path('reads/add', views.add_book_and_review),
     path('reads/<int:book_id>/delete', views.delete_book),
-    path('reads/<int:book_id>/', views.book_info_page),
-    path('reads/add_review', views.add_user_review)
+    path('<int:book_id>', views.book_info_page),
+    path('<int:book_id>/add_review', views.add_user_review),
+    path('/<int:user_id>', views.user_info_page),
 ]
--- reads_app/views.py
+++ reads_app/views.py
@@ -4,12 +4,11 @@ from .models import Book, Author, Review
 from registration_app.models import User
 
 def index(request):
-    
-            
+
     context = {
         "user": User.objects.get(id = request.session['user_id']),
-        "book": Book.objects.all(),
-        "review": Book.objects.get(pk=1).book_reviews.all()
+        "recent_reviews": Review.objects.all().order_by('-created_at')[0:3],
+        "books": Book.objects.all()
     }
     return render(request, "books_home.html", context)
 
@@ -23,35 +22,41 @@ def add_book_page(request):
 def book_info_page(request, book_id):
     context = {
         "user": User.objects.get(id = request.session['user_id']),
-        "book": Book.objects.get(id = book_id),
+        "book": Book.objects.get(id = book_id)
     }
     return render(request, 'book_info.html', context)
 
+def user_info_page(request, user_id):
+    context = {
+        "user": User.objects.get(id = request.session['user_id']),
+        "author": Author.objects.all(),
+    }
+    return render(request, 'user_info.html', context)
+
 def add_book_and_review(request):
     
-    Review.objects.create(
-        content = request.POST['content'],
-        rating = request.POST['rating'],
-        poster = User.objects.get(id = request.session['user_id']),
-        
+    Author.objects.create(
+        name = request.POST['name'],
     )
-    latest_review = Review.objects.last()
     
+    latest_author = Author.objects.last()
 
     Book.objects.create(
         title = request.POST['title'],
         contributor = User.objects.get(id = request.session['user_id']),
-        review = latest_review,
+        author = latest_author
     )
 
     latest_book = Book.objects.last()
 
-    Author.objects.create(
-        name = request.POST['name'],
-        book_written = latest_book,
+    Review.objects.create(
+        content = request.POST['content'],
+        rating = request.POST['rating'],
+        poster = User.objects.get(id = request.session['user_id']),
+        book = latest_book
     )
-    return redirect('/reads')
 
+    return redirect('/reads')
 
 def delete_book(request, book_id):
     book = Book.objects.get(id = book_id)
@@ -59,10 +64,11 @@ def delete_book(request, book_id):
         book.delete()
     return redirect('/reads')
     
-def add_user_review(request):
+def add_user_review(request, book_id):
     Review.objects.create(
         content = request.POST['content'],
         rating = request.POST['rating'],
         poster = User.objects.get(id = request.session['user_id']),
+        book = Book.objects.get(id = book_id)
     )
-    return redirect(f'reads/{{Book.id}}')
+    return redirect(f'/reads/{book_id}')
